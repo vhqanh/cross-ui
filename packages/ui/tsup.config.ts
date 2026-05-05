2bc6be0d09146e30679f52192dee17301de5f5f0
vhqanh <anh.quynh.vu@aioz.io>
2026-05-05 11:36:54 +0700

    Update package.json and tsup.config.ts for improved module exports
    - Changed the paths in package.json for `require` and `import` to point to the correct index files in the tokens directory.
    - Updated the entry point in tsup.config.ts to reflect the new structure for the tokens module, enhancing clarity and organization.

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
-    tokens: 'src/tokens/index.ts',
+    'tokens/index': 'src/tokens/index.ts',
   },
   format: ['cjs', 'esm'],
   dts: false,
